5d4b7e3949665cde7ddcea5264a15d173deb30f4
Adibah Mahima <adibahmahima@gmail.com>
2024-07-16 17:40:55 +0600

    modefied about.php and added new images

--- about.php
+++ about.php
@@ -37,26 +37,34 @@
   </ul>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="image/crsl1.png" alt="Los Angeles" width="789" height="442">
+      <img src="image/crsl1.png" alt="Experts" width="789" height="442">
       <div class="carousel-caption">
         <h3>Learn from Expert!</h3>
         <p>We have many experts in hackathon who can help you with any problem.</p>
       </div>   
     </div>
     <div class="carousel-item">
-      <img src="image/crsl2.png" alt="Chicago" width="789" height="442">
+      <img src="image/crsl2.png" alt="Problemsets" width="789" height="442">
       <div class="carousel-caption">
-        <h3>Easy Problemset.</h3>
+        <h3>Solve Easy Problems!</h3>
         <p>There are small problems on different topics for the begginers.</p>
       </div>   
     </div>
     <div class="carousel-item">
-      <img src="image/crsl3.png" alt="New York" width="789" height="442">
+      <img src="image/crsl4.jpg" alt="Discuss" width="789" height="442">
+      <div class="carousel-caption">
+        <h3>Discuss with peers!</h3>
+        <p>You can discuss about any topics with your peers.</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/crsl3.png" alt="Participate" width="789" height="442">
       <div class="carousel-caption">
         <h3>Participate!</h3>
         <p>We arrange begginer friendly and regular Hackathons. Participate, Learn and Win!</p>
       </div>   
     </div>
+    
   </div>
   <a class="carousel-control-prev" href="#demo" data-slide="prev">
     <span class="carousel-control-prev-icon"></span>
@@ -65,6 +73,63 @@
     <span class="carousel-control-next-icon"></span>
   </a>
 </div>
+
+<section class="firstcontent">
+<div class="firstcontent-text">
+  <h2>About our Platform</h2>
+  <p>Want to turn your coding passion into reality? 
+    Hackathon Hub empowers anyone, beginner or pro, to become a tech whiz. 
+    We provide a dynamic community, expert guidance, and engaging challenges to fuel your coding journey.</p>
+  <p>Here, you'll be surrounded by passionate peers, brainstorming alongside industry experts and mentors. 
+    They'll share their knowledge and guide you through the exciting world of coding. 
+    We offer beginner-friendly problem sets, a fun way to build confidence and coding fundamentals before tackling complex projects.</p>
+    <p>As you progress, delve deeper with our comprehensive online courses tailored to your interests. 
+      Learn new coding languages, frameworks, and development tools. But learning isn't solitary! 
+      Our platform fosters a vibrant community. Connect with fellow coders, discuss ideas, share challenges, and learn from each other. 
+      Build connections and find teammates for upcoming hackathons!</p>
+      <p>We host exciting hackathons with engaging themes regularly. 
+        Showcase your skills, collaborate with like-minded individuals, and bring your innovative ideas to life. 
+        Hackathon Hub is your springboard for innovation. 
+        We offer the resources, support, and inspiration to turn your coding dreams into reality. 
+        Join us and unleash your inner coding genius!</p>
+</div>
+</section>
+
+<section class="features1">
+        <div class="feature1-container">
+
+            <div class="feature1-box">
+                <div class="f1-img">
+                  <a href="qna.php"><img src="image/add1.jpg"/></a>
+                </div>
+            </div>
+
+            <div class="feature1-box">
+                <div class="f1-img">
+                    <img src="image/add2.jpg"/>
+                </div>
+            </div>
+
+            <div class="feature1-box">
+                <div class="f1-img">
+                    <img src="image/add3.jpg"/>
+                </div>
+            </div>
+
+            <div class="feature1-box">
+                <div class="f1-img">
+                    <img src="image/add4.jpg"/>
+                </div>
+            </div>
+
+            <div class="feature1-box">
+                <div class="f1-img">
+                    <img src="image/add5.jpg"/>
+                </div>
+            </div>
+
+        </div>
+    </section>
 
     
 
